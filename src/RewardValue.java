f2b702a901de85249b07420d8a6d7525c9896080
Prajna-24 <153409636+Prajna-24@users.noreply.github.com>
2024-07-03 12:52:04 +0530

    RewardValue.java

--- src/RewardValue.java
+++ src/RewardValue.java
@@ -2,28 +2,39 @@ package main.java;
 
 public class RewardValue 
 {
-	double cash;//cash in double
-	int miles;//miles in int
+	 double cash;//cash in double
+	 int miles;//miles in int
 	
 	public RewardValue(double cash)//constructor for cash
 	{
 		this.cash=cash;
+		ConvertCashIntoMiles(cash);
 	}
-	public RewardValue(int miles)//constructor for mile
+	public RewardValue(int miles)//constructor for miles
 	{
 		this.miles=miles;
+		ConvertMilesIntoCash(miles);
 	}
 	
 	public int getMilesValue()
-	//function will return cash to miles value
 	{
-		miles=(int)(cash/0.0035);
 		return miles;
 	}
-	
-	public double getCashValue()//function will return miles to cash value
+	public double getCashValue()
 	{
-		cash=0.0035*miles;
 		return cash;
 	}	
+	
+	public int ConvertCashIntoMiles(double cash)
+	//function will return cash to miles value
+	{
+		this.miles=(int)(cash/0.0035);
+		return this.miles;
+	}
+	public double ConvertMilesIntoCash(int miles)
+	//function will return miles to cash value
+	{
+		this.cash=0.0035*miles;
+		return this.cash;
+	}
 }
